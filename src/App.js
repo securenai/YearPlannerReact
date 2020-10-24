4f5f165972dd6b37b1a50412bf18cc92ee419150
乃軒 securenai <michaelnccu@gmail.com>
2020-10-24 17:41:48 +0800

    added css loader to handle css

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+	return (
+		<div>
+			<h1>hello world!!</h1>
+			<p>this is some text!!!</p>
+		</div>
+	);
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = () => {
-	return (
-		<div>
-			<h1>hello world!!</h1>
-			<p>this is some text</p>
-		</div>
-	);
-};
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-export default App;
